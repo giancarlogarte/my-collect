e0af8d20f8b30e247873d55bc7d34a2dca466a4e
giancarlogarte <giancarlogarte@gmail.com>
2021-02-25 15:41:08 -0500

    commit tree e6ece32abc0b9c42ebb377feb98ded3e29f935d4

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+require 'pry'
 
+def my_collect (collection)
+    array = []
+    counter = 0 
+    while counter < collection.length
+        array << yield(collection[counter])
+        counter +=1
+    end
+    array
+end
 
